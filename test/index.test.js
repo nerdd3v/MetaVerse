af14dd26be066357e3068c4e355a70bb80cc1d66
n3rddev <n3rddev.code@gmail.com>
2025-08-31 12:24:30 +0530

    reinitialised with writing the tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,144 +2,30 @@ const { default: axios } = require("axios");
 const backendURL = 'http://localhost:3000'
 
 
-
 describe('Authentication',()=>{
-  const username = "n3rddev" + Math.random();
-  const password = '12345';
-  
-  test('user is able to signup',async ()=>{
-    const response = await axios.post(`${backendURL}/api/v1/signup`, {
-      username,
-      password,
-      type:'admin'
-    })
-    expect(response.statusCode).toBe(200);
-
-    const responseUpdated = await axios.post(`${backendURL}/api/v1/signup`, {
-      username,
-      password,
-      type:'admin'
-    })
-    expect(responseUpdated.statusCode).tobe(400);
-  })
-
-  test('signup fails if the username is empty', async()=>{
+  test('user signup with correct username, password and role', async()=>{
     const username = 'saket'+Math.random();
-    const password = "12345";
+    const password = '12345';
+    const role = 'admin';
 
-    const response = axios.post(`${backendURL}/api/v1/signup`,{
+    const response = await axios.post(`${backendURL}/api/v1/signup`,{
+      username,
       password,
-      type: 'admin'
+      role 
     })
-    expect(response.statusCode).toBe(400)
+
+    expect(response.statusCode).toBe(200);
+    expect(response.data.userId).toBeDefined();
   })
 
-  test('signin succeds if correct username & password are correct', async()=>{
-    const username = 'saket'+"LLLL"+Math.random();
+  test('signin fails if username is not provided',async()=>{
+    const username = 'saket'+Math.random();
     const password = '12345';
-
-    await axios.post(`${backendURL}/api/v1/signup`,{
-      username,
-      password
-    })
-
+    const role = 'admin';
     const response = await axios.post(`${backendURL}/api/v1/signin`,{
-      username,
-      password
+      password,
+      role 
     })
-
-    expect(response.statusCode).toBe(200);  
-    expect(response.token).toBeDefined();
-  })
-
-    test('signin fails if correct username & password are incorrect', async()=>{
-    const username = 'saket'+"LLLL"+Math.random();
-    const password = '12345';
-
-    await axios.post(`${backendURL}/api/v1/signup`,{
-      username,
-      password
-    })
-
-    const response = await axios.post(`${backendURL}/api/v1/signin`,{
-      username:"wrong-username",
-      password
-    })
-
-    expect(response.statusCode).toBe(403);  
-    expect(response.token).toBeUndefined();
-  })
-
-})
-
-
-describe('User-avatar',()=>{
-  let token = '';
-  let avatarId = '';
-
-
-  beforeAll(async()=>{
-    const username = 'dhruv'+Math.random();
-    const password = 'harDick';
-
-    await axios.post(`${backendURL}/api/v1/signup`,{
-      username,
-      password 
-    });
-
-    const reponse = await axios.post(`${backendURL}/api/v1/signin`,{
-      username,
-      password
-    });
-
-    token = reponse.data.roken;
-
-    const avatarResponse = await axios.post(`${backendURL}/api/v1/avatar`,{
-      imageUrl: "xyz.com",
-      name:"trikky"
-    })
-
-    avatarId = avatarResponse.data.avatarId;
-  })
-
-  test('user cant update their avatar with a wrong avatarID', async()=>{
-    const avatarId = 'wrongAvatarID'
-    const response = await axios.post(`${backendURL}/api/v1/metadata`,{
-      avatarId
-    },{
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
-    })
-    expect(response.statusCode).toBe(400)
-  })
-
-  test('user can update their avatar with the right avatar id', async()=>{
-    const response = await axios.post(`${backendURL}/api/v1/metadata`,{
-      avatarId
-    },{
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
-    })
-    expect(response.statusCode).toBe(200)
-  })
-
-  test('user does not send authorization header', async()=>{
-    const response = axios.post(`${backendURL}/api/vi/metadata`,{
-      avatarId
-    })
-    expect(response.statusCode).toBe(403);
+    expect(response.statusCode).toBe(400);
   })
 })
-
-describe('user-metadata',()=>{
-  const userId ='';
-  const token = '';
-
-  beforeAll(async()=>{
-    const username = "bigdihh"+Math.random();
-  })
-  test()
-})
-//describe blocks
